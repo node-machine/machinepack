0fd527ba67c475807e3193bf2203523105b60d62
Mike McNeil <mike@balderdash.co>
2014-12-28 17:20:42 -0600

    Build exec into a proper machine.

--- bin/machinepack-exec.js
+++ bin/machinepack-exec.js
@@ -7,7 +7,7 @@
 var program = require('commander');
 var chalk = require('chalk');
 var Machinepacks = require('machinepack-machinepacks');
-
+var Machine = require('machine');
 
 
 program
@@ -23,15 +23,106 @@ if (!program.args[0]) {
 var identity = program.args[0];
 
 
-fn({
+(Machine.build({
+  inputs: {
+    identity: {
+      example: 'do-stuff'
+    },
+    dir: {
+      example: '/Users/mikermcneil/machinepack-foo/'
+    }
+  },
+  defaultExit: 'success',
+  exits: {
+    success: {
+      example: {
+        exit: 'success',
+        jsonValue: '{"stuff": "things"}',
+        void: false
+      }
+    },
+    error: {},
+    invalidMachine: {}
+  },
+  fn: function (inputs, exits, env){
+
+    // Dependencies
+    var Path = require('path');
+    var _ = require('lodash');
+    var Filesystem = require('machinepack-fs');
+    var Machinepacks = require('machinepack-machinepacks');
+
+    Machinepacks.getMachinesDir({
+      dir: Path.resolve(process.cwd(), inputs.dir)
+    }).exec({
+      error: function (err){
+        return exits.error(err);
+      },
+      success: function (pathToMachines){
+
+        Filesystem.readJson({
+          source: Path.resolve(process.cwd(), 'package.json')
+        }).exec({
+          error: function (err){
+            return exits.error(err);
+          },
+          success: function (jsonData){
+            try {
+              if (!_.contains(jsonData.machinepack.machines, inputs.identity)) {
+                return exits.notFound();
+              }
+              jsonData.machinepack.machines = _.difference(jsonData.machinepack.machines, [inputs.identity]);
+            }
+            catch (e) {
+              return exits.error(e);
+            }
+
+            var pathToMachine = Path.resolve(pathToMachines, inputs.identity+'.js');
+
+            env.log();
+            // console.log(chalk.red('TODO: .exec() implementation is not finished yet!'));
+
+
+            // env.log(chalk.gray('(i.e. interactive prompt that asks for input values...'));
+            var configuredInputValues = {};
+
+            Machinepacks.runMachine({
+              path: pathToMachine,
+              inputs: configuredInputValues
+            }).exec({
+              error: function (err){
+                return exits.error(err);
+              },
+              invalidMachine: function (err){
+                return exits.invalidMachine(err);
+              },
+              success: function (result){
+                // env.log(chalk.gray('  then a log explaining which exit was traversed and the return value, if relevant)'));
+                return exits.success(result);
+              }
+            });
+
+          },
+        });
+      }
+    });
+  }
+}))({
   identity: identity,
   dir: process.cwd()
-}, {
+})
+.setEnvironment({
+  log: console.log
+})
+.exec({
   error: function (err){
     console.error('Unexpected error occurred:\n',err);
   },
   notFound: function (){
     console.error('Cannot run machine `'+chalk.red(identity)+'`.  No machine with that identity exists in this machinepack.');
+  },
+  invalidMachine: function (err){
+    console.error('Cannot run machine `'+chalk.red(identity)+'`. Machine is invalid.  Error details:\n',err);
   },
   success: function (result){
     console.log('* * * DONE * * *');
@@ -46,73 +137,6 @@ fn({
       console.log('No value was returned.');
     }
   }
-}, {
-  log: console.log
 });
 
 
-
-
-
-function fn (inputs, exits, env){
-
-  // Dependencies
-  var Path = require('path');
-  var _ = require('lodash');
-  var Filesystem = require('machinepack-fs');
-  var Machinepacks = require('machinepack-machinepacks');
-
-  Machinepacks.getMachinesDir({
-    dir: Path.resolve(process.cwd(), inputs.dir)
-  }).exec({
-    error: function (err){
-      return exits.error(err);
-    },
-    success: function (pathToMachines){
-
-      Filesystem.readJson({
-        source: Path.resolve(process.cwd(), 'package.json')
-      }).exec({
-        error: function (err){
-          return exits.error(err);
-        },
-        success: function (jsonData){
-          try {
-            if (!_.contains(jsonData.machinepack.machines, inputs.identity)) {
-              return exits.notFound();
-            }
-            jsonData.machinepack.machines = _.difference(jsonData.machinepack.machines, [inputs.identity]);
-          }
-          catch (e) {
-            return exits.error(e);
-          }
-
-          var pathToMachine = Path.resolve(pathToMachines, inputs.identity+'.js');
-
-          env.log();
-          // console.log(chalk.red('TODO: .exec() implementation is not finished yet!'));
-
-
-          // env.log(chalk.gray('(i.e. interactive prompt that asks for input values...'));
-          var configuredInputValues = {};
-
-          Machinepacks.runMachine({
-            path: pathToMachine,
-            inputs: configuredInputValues
-          }).exec({
-            error: function (err){
-              return exits.error(err);
-            },
-            success: function (result){
-              // env.log(chalk.gray('  then a log explaining which exit was traversed and the return value, if relevant)'));
-              return exits.success(result);
-            }
-          });
-
-        },
-      });
-    }
-  });
-
-
-}
