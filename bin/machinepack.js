6d4796a3671eb91d8d33d088db6c9fda7bb8ccde
Mike McNeil <mike@balderdash.co>
2014-12-28 13:02:36 -0600

    Set up aliases for commands.

--- bin/machinepack.js
+++ bin/machinepack.js
@@ -27,6 +27,31 @@ if (program.args.length === 0) {
 
 
 // Aliases:
-if (program.args[0] === 'machines') {
-  program.args[0] = 'ls';
+if (program.args[0] === 'list' || program.args[0] === 'machines') {
+  return _alias('ls');
+}
+if (program.args[0] === 'initialize' || program.args[0] === 'new') {
+  return _alias('init');
+}
+if (program.args[0] === 'remove') {
+  return _alias('rm');
+}
+if (program.args[0] === 'run') {
+  return _alias('exec');
+}
+if (program.args[0] === 'touch') {
+  return _alias('add');
+}
+if (program.args[0] === 'move') {
+  return _alias('mv');
+}
+if (program.args[0] === 'copy') {
+  return _alias('cp');
+}
+
+
+
+function _alias (aliasFor){
+  process.argv.splice(process.argv.indexOf(program.args[0]),1);
+  require('./machinepack-'+aliasFor);
 }
