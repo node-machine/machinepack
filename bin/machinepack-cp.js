248d026bf5e9e5ece370fe9020fb943d3bb3e14a
Mike McNeil <mike@balderdash.co>
2014-12-28 12:29:22 -0600

    Pull out cp and mv

--- bin/machinepack-cp.js
+++ bin/machinepack-cp.js
@@ -16,69 +16,29 @@ program
   .parse(process.argv);
 
 
+var originalIdentity = program.args[0];
 if (!program.args[0]) {
   console.error('`originalIdentity` required');
   process.exit(1);
 }
+
+var newIdentity = program.args[1];
 if (!program.args[1]) {
   console.error('`newIdentity` required');
   process.exit(1);
 }
 
 
-var originalIdentity = program.args[0];
-var newIdentity = program.args[1];
 
-Machinepacks.getMachinesDir({
-  dir: Path.resolve(process.cwd())
+Machinepacks.copyMachine({
+  originalIdentity: originalIdentity,
+  newIdentity: newIdentity,
+  dir: process.cwd()
 }).exec({
   error: function (err){
     console.error('Unexpected error occurred:\n',err);
   },
-  success: function (pathToMachines){
-
-    Filesystem.cp({
-      source: Path.resolve(pathToMachines, originalIdentity+'.js'),
-      destination: Path.resolve(pathToMachines, newIdentity+'.js')
-    }).exec({
-
-      error: function (err){
-        console.error('Unexpected error occurred:\n',err);
-      },
-
-      success: function (){
-
-        Filesystem.readJson({
-          source: Path.resolve(process.cwd(), 'package.json')
-        }).exec({
-          error: function (err){
-            console.error('Unexpected error occurred:\n',err);
-          },
-          success: function (jsonData){
-            try {
-              jsonData.machinepack.machines = _.union(jsonData.machinepack.machines, [newIdentity]);
-            }
-            catch (e) {
-              console.error('Unexpected error occurred:\n',err);
-              return;
-            }
-            Filesystem.writeJson({
-              json: jsonData,
-              destination: Path.resolve(process.cwd(), 'package.json'),
-              force: true
-            }).exec({
-              error: function (err){
-                console.error('Unexpected error occurred:\n',err);
-              },
-              success: function (){
-                // Done.
-                console.log('Copied: `%s` to new machine: `%s`', originalIdentity, newIdentity);
-              }
-            });
-          }
-        });
-      },
-    });
+  success: function (){
+    console.log('Copied: `%s` to new machine: `%s`', originalIdentity, newIdentity);
   }
 });
-
--- bin/machinepack-mv.js
+++ bin/machinepack-mv.js
@@ -29,74 +29,15 @@ if (!newIdentity) {
 }
 
 
-
-// TODO:
-// Machinepacks.moveMachine({
-//   source: originalIdentity,
-//   destination: newIdentity,
-//   dir: process.cwd()
-// }).exec({
-//   error: function (err){
-//     console.error('Unexpected error occurred:\n',err);
-//   },
-//   success: function (){
-//     console.log('Machine with former identity: `%s` is now: `%s`', originalIdentity, newIdentity);
-//   }
-// });
-
-
-
-Machinepacks.getMachinesDir({
-  dir: Path.resolve(process.cwd())
+Machinepacks.renameMachine({
+  originalIdentity: originalIdentity,
+  newIdentity: newIdentity,
+  dir: process.cwd()
 }).exec({
   error: function (err){
     console.error('Unexpected error occurred:\n',err);
   },
-  success: function (pathToMachines){
-
-    Filesystem.mv({
-      source: Path.resolve(pathToMachines, originalIdentity+'.js'),
-      destination: Path.resolve(pathToMachines, newIdentity+'.js')
-    }).exec({
-
-      error: function (err){
-        console.error('Unexpected error occurred:\n',err);
-      },
-
-      success: function (){
-
-        Filesystem.readJson({
-          source: Path.resolve(process.cwd(), 'package.json')
-        }).exec({
-          error: function (err){
-            console.error('Unexpected error occurred:\n',err);
-          },
-          success: function (jsonData){
-            try {
-              jsonData.machinepack.machines = _.difference(jsonData.machinepack.machines, [originalIdentity]);
-              jsonData.machinepack.machines = _.union(jsonData.machinepack.machines, [newIdentity]);
-            }
-            catch (e) {
-              console.error('Unexpected error occurred:\n',err);
-              return;
-            }
-            Filesystem.writeJson({
-              json: jsonData,
-              destination: Path.resolve(process.cwd(), 'package.json'),
-              force: true
-            }).exec({
-              error: function (err){
-                console.error('Unexpected error occurred:\n',err);
-              },
-              success: function (){
-                // Done.
-                console.log('Machine with former identity: `%s` is now: `%s`', originalIdentity, newIdentity);
-              }
-            });
-          }
-        });
-      },
-    });
+  success: function (){
+    console.log('Machine with former identity: `%s` is now: `%s`', originalIdentity, newIdentity);
   }
 });
-
